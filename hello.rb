ac9132ae5a952496aa6d2f58b1cff08e4b8c9f42
masayuki oguni <masayuki.oguni.dev@gmail.com>
2012-03-26 14:37:50 +0900

    試作完了

--- hello.rb
+++ hello.rb
@@ -1,5 +1,6 @@
 require 'rubygems'
 require 'sinatra'
+require 'wilksformula'
 
 # reloader
 set :enviroment, :development
@@ -39,6 +40,65 @@ get '/api/reverseformula/' do
   params.to_s
   #{}"Hellxxxxxo #{params["a"]} #{params["b"]}!"
 end
+
+get '/wilksformula/' do
+  # matches /say/hello/to/world
+  #params[:splat]
+  #params.to_s
+  wf = WilksFormula.new()
+  weight = params["weight"].to_f()
+  total = params["total"].to_f()
+  res = wf.get_men_formula(weight,total)
+  "Hellxxxxxo #{weight} #{total} #{res}"
+end
+
+enable :sessions
+get '/session' do
+  session["count"] ||= 0
+  session["count"] += 1
+  "count: #{session['count']}"
+end
+
+require 'json'
+get '/api/items.json' do
+    content_type :json
+    item = {"apple"=>150, "banana"=>300, "lemon"=>300}
+    JSON.unparse(item)
+end
+
+
+
+get '/wilksformula/json' do
+  # matches /say/hello/to/world
+  item = {}
+  res = 0
+  if !params.has_key?('weight')
+    res = 1
+    item['error_message'] = 'not found weight param'
+  end
+    
+  if !params.has_key?('total')
+    res = 1
+    item['error_message'] = 'not found total param'
+  end
+  
+  item['result'] = res
+
+  if res == 0
+    wf = WilksFormula.new()
+
+    weight = params["weight"].to_f()
+    total = params["total"].to_f()
+    factor = wf.get_men_factor(weight)
+    formula  = wf.get_men_formula(weight,total)
+
+    res_item = {"weight"=>weight, "total"=>total, "factor"=>factor,"formula"=>formula}
+    item["data"] = res_item
+  end
+  content_type :json
+  JSON.unparse(item)
+end
+
 
 
 
